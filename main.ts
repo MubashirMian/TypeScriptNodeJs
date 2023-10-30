16d6ea34027900e268a3246d9da7bffabb7cf91c
MubashirMian <138796628+MubashirMian@users.noreply.github.com>
2023-10-30 17:36:34 +0500

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,114 +1,42 @@
-import * as fs from 'fs';
-import inquirer  from 'inquirer';
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import { quizQuestions } from "./questions.js";
 
-const todoFile = 'todo.json';
-
-interface Task {
-  id: number;
-  task: string;
-  completed: boolean;
-}
-
-// Function to load tasks from the JSON file
-function loadTasks(): Task[] {
-  try {
-    const data = fs.readFileSync(todoFile, 'utf8');
-    return JSON.parse(data);
-  } catch (error) {
-    return [];
-  }
-}
-
-// Function to save tasks to the JSON file
-function saveTasks(tasks: Task[]) {
-  fs.writeFileSync(todoFile, JSON.stringify(tasks, null, 2), 'utf8');
-}
-
-// Function to display the current tasks
-function displayTasks() {
-  const tasks = loadTasks();
-
-  if (tasks.length === 0) {
-    console.log('No tasks in the to-do list.');
-  } else {
-    console.log('To-Do List:');
-    tasks.forEach((task, index) => {
-      console.log(`${index + 1}. [${task.completed ? 'X' : ' '}] ${task.task}`);
-    });
-  }
-}
-
-// Function to add a new task
-async function addTask() {
-  const { task } = await inquirer.prompt([
-    {
-      type: 'input',
-      name: 'task',
-      message: 'Enter a new task:',
-    },
-  ]);
-
-  const tasks = loadTasks();
-  const newTask: Task = {
-    id: Date.now(),
-    task: task,
-    completed: false,
-  };
-  tasks.push(newTask);
-  saveTasks(tasks);
-  console.log('Task added to the to-do list.');
-}
-
-// Function to remove a task
-async function removeTask() {
-  const tasks = loadTasks();
-
-  if (tasks.length === 0) {
-    console.log('No tasks to remove.');
-    return;
-  }
-
-  const { taskIndex } = await inquirer.prompt([
-    {
-      type: 'list',
-      name: 'taskIndex',
-      message: 'Select a task to remove:',
-      choices: tasks.map((task, index) => `${index + 1}. ${task.task}`),
-    },
-  ]);
-
-  const indexToRemove = parseInt(taskIndex.split('.')[0]) - 1;
-  tasks.splice(indexToRemove, 1);
-  saveTasks(tasks);
-  console.log('Task removed from the to-do list.');
-}
-
-async function main() {
-  while (true) {
-    const { action } = await inquirer.prompt([
-      {
-        type: 'list',
-        name: 'action',
-        message: 'Choose an action:',
-        choices: ['Add a task', 'Remove a task', 'Show tasks', 'Exit'],
-      },
-    ]);
-
-    switch (action) {
-      case 'Add a task':
-        await addTask();
-        break;
-      case 'Remove a task':
-        await removeTask();
-        break;
-      case 'Show tasks':
-        displayTasks();
-        break;
-      case 'Exit':
-        process.exit();
+function shuffleArray(array: any[]) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
     }
+
+  }
+async function Quiz(){
+  let score = 0
+  for (let i = 0; i < quizQuestions.length; i++) {
+    console.log("Question "+ (i+1) +"/5")
+    const answers = await inquirer.prompt([{
+        type :'list',
+        name :'Answer',
+        message : quizQuestions[i].question,
+        choices : quizQuestions[i].choices
+    },])
+
+    if(answers.Answer === quizQuestions[i].correctAnswer){
+        console.log("Your answer is Correct!")
+        score++
+    }
+    else{console.log("Your answer is Incorrect")}
+  }
+  let percent = score*20
+  if(percent>=60){
+  console.log("Congrats! your score is " + percent+" %")
+  }
+  else{
+    console.log("OOPS! your score is "+percent+" %" )
   }
 }
 
-main();
 
+console.log("\nThis quiz consists of 5 questions\n")
+shuffleArray(quizQuestions);
+Quiz();
+  
